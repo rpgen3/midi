9c036fbf10d8529c09ecbe9e00c8aa827440588d
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-26 08:53:36 +0900

    Update eventMax.mjs

--- export/eventMax.mjs
+++ export/eventMax.mjs
@@ -1,10 +1,11 @@
+let xx, yy;
 class Ph {
     constructor(x, events, i){
         this.head = `#PH${i} tm:0,${i ? 'sw:99,g:,' : ''}`;
         this.body = events;
         this.foot = `#PHEND${i}`;
         const next = (i + 1) % 4;
-        this.ch_ph = `#CH_PH\np:${next},x:${next ? x : x + 1},y:0,`;
+        this.ch_ph = `#CH_PH\np:${next},x:${(next ? x : x + 1) + xx},y:${yy},`;
     }
     toStr(){
         return [
@@ -16,7 +17,7 @@ class Ph {
 }
 class Event {
     constructor(x, events){
-        this.head = `#EPOINT tx:${x},ty:0,`;
+        this.head = `#EPOINT tx:${x + xx},ty:${yy},`;
         this.body = [];
         this.foot = '#END';
         for(let i = 0; i < 4; i++){
@@ -37,7 +38,8 @@ export class EventMax {
     constructor(already){
         this.max = 96 - already;
     }
-    make(events){
+    make(events, x = 0, y = 0){
+        [xx, yy] = [x, y];
         const arr = [];
         for(let i = 0; i < this.max; i++){
             const j = i * 400;
