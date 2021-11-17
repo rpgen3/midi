79d4d7e6045d892dd0cd84a40f1686ebeb73090d
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-11-17 19:57:18 +0900

    Update rpgen.mjs

--- export/rpgen.mjs
+++ export/rpgen.mjs
@@ -2,12 +2,21 @@ import {getScript} from 'https://rpgen3.github.io/mylib/export/import.mjs';
 getScript('https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.4.4/lz-string.min.js');
 const toStr = func => String(func).replace(/\/\/.*\n/g,'');
 export const set = input => {
-    const data = LZString.compressToEncodedURIComponent(input),
-          code = () => `avascript:(()=>{var m='${JSON.stringify(input)}';window.getCurrentMapText=()=>JSON.stringify(m);$('#idBtnDqEditEnd').click();})();`;
+    const pass = Date.now().toString(16),
+          mapText = input.replace(/\n/g, pass);
+    const code = () => `avascript:(()=>{
+    const m='${JSON.stringify(input)}';
+    const copy = window.getCurrentMapText;
+    window.getCurrentMapText = () => {
+    window.getCurrentMapText = copy;
+    return ${mapText}.replace(/${pass}/g, '\n');
+    };
+    $('#idBtnDqEditEnd').click();
+    })();`;
     return {
         valueOf: code,
         toString: code,
-        data
+        data: LZString.compressToEncodedURIComponent(input)
     };
 };
 export const get = () => "avascript:(" + toStr(getFile) + ")();";
